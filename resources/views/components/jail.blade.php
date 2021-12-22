55488c7a67734321294f7b70b1fdf9ac6abf4e2c
Penfu <sypenfu@gmail.com>
2021-12-22 11:05:24 +0100

    Fix jail overflow and contries rendering

--- resources/views/components/jail.blade.php
+++ resources/views/components/jail.blade.php
@@ -1,4 +1,4 @@
-<div class="snap-center w-full px-4 py-2 bg-white rounded shadow">
+<div class="grow snap-center min-w-max w-auto px-4 py-2 bg-white rounded shadow">
     <h3 class="border-b border-gray-200 text-2xl">{{ $jail }} - {{ $options['ban_count'] }}</h3>
     <div class="w-full flex py-2">
         <div class="grow">
@@ -6,7 +6,8 @@
             <span class="block">{{ __('maxretry') }}</span>
         </div>
         <div>
-            <span class="block font-bold">{{Carbon\CarbonInterval::seconds($options['bantime'])->cascade()->forHumans();}}</span>
+            <span
+                class="block font-bold">{{ Carbon\CarbonInterval::seconds($options['bantime'])->cascade()->forHumans() }}</span>
             <span class="block font-bold">{{ $options['maxretry'] }}</span>
         </div>
     </div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,24 +5,23 @@
         </h2>
     </x-slot>
 
-    <div class="py-8">
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div class="space-y-8">
-                <div>
-                    <h2 class="text-2xl mb-4">{{ __('Jails') }}</h2>
-                    <div class="flex items-stretch space-x-12 snap-x overflow-x-auto">
-                        @foreach ($jails as $key => $options)
-                            <x-jail jail="{{ $key }}" :options="$options" />
-                        @endforeach
-                    </div>
-                </div>
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="py-8">
+            <h2 class="text-2xl mb-4">{{ __('Jails') }}</h2>
+            <div class="flex items-stretch space-x-12 snap-x overflow-x-auto">
+                @foreach ($jails as $key => $options)
+                    <x-jail jail="{{ $key }}" :options="$options" />
+                @endforeach
             </div>
         </div>
-    </div>
 
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-wrap">
-        @foreach ($stats as $stat)
-            <x-country-stat :stat="$stat"/>
-        @endforeach
+        <div class="py-8">
+            <h2 class="text-2xl mb-4">{{ __('Contries with most bans') }}</h2>
+            <div class="grid grid-cols-3 gap-4">
+                @foreach ($stats as $stat)
+                    <x-country-stat :stat="$stat" />
+                @endforeach
+            </div>
+        </div>
     </div>
 </x-app-layout>
