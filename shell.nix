e0a0bc411df7aa7567cb1839a74d9ec3454aca2c
thynkon <thynkon@protonmail.com>
2022-01-19 10:12:10 +0000

    Bump PHP version to 8.0 on NixOS

--- shell.nix
+++ shell.nix
@@ -1,25 +1,26 @@
-{ pkgs ? import <nixpkgs> {}, unstable ? import <nixos-unstable> {} }:
+{ pkgs ? import <nixpkgs> { }, unstable ? import <nixos-unstable> { } }:
 
 let
-        php' = pkgs.php74.buildEnv {
-            extraConfig = ''
-            extension=${unstable.php74Extensions.mongodb}/lib/php/extensions/mongodb.so
-            [xdebug]
-            zend_extension=${pkgs.php74Extensions.xdebug}/lib/php/extensions/xdebug.so
-                    xdebug.mode=debug
-                    xdebug.client_host=127.0.0.1
-                    xdebug.client_port=9003'';
-        };
-        mongodb = pkgs.mongodb;
+  php' = pkgs.php80.buildEnv {
+    extraConfig = ''
+      extension=${unstable.php80Extensions.mongodb}/lib/php/extensions/mongodb.so
+      # memory_limit = 2048M
+      [xdebug]
+      zend_extension=${pkgs.php80Extensions.xdebug}/lib/php/extensions/xdebug.so
+              xdebug.mode=debug
+              xdebug.client_host=127.0.0.1
+              xdebug.client_port=9003'';
+  };
+  mongodb = pkgs.mongodb;
 in
-        pkgs.mkShell {
-                buildInputs = [
-                        php'
-                        php'.packages.composer
-                        mongodb
-                ];
-                # does not work if mongo is not installed system-wise
-                shellHook = ''
-                systemctl start mongodb.service
-                '';
+pkgs.mkShell {
+  buildInputs = [
+    php'
+    php'.packages.composer
+    mongodb
+  ];
+  # does not work if mongo is not installed system-wise
+  shellHook = ''
+    systemctl start mongodb.service
+  '';
 }
