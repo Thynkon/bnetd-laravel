ed97f09f77f658798badab1c683a1fd5851e5c8a
Penfu <sypenfu@gmail.com>
2022-01-19 11:02:22 +0100

    Extract protocol traffic list in Livewire

--- app/Http/Controllers/NetworkProtocolController.php
+++ app/Http/Controllers/NetworkProtocolController.php
@@ -2,15 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Ban;
-use App\Models\NicProtocolTraffic;
-
 class NetworkProtocolController extends Controller
 {
     public function index()
     {
-        $protocols = NicProtocolTraffic::today()->orderByDesc()->get();
-
-        return view('network-protocol')->with('protocols', $protocols);
+        return view('network-protocol');
     }
 }
--- resources/views/network-protocol.blade.php
+++ resources/views/network-protocol.blade.php
@@ -163,65 +163,7 @@
                                     <livewire:protocol-traffic-filter />
 
                                     <!-- Product grid -->
-                                    <div class="lg:col-span-3">
-                                        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                                            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                                                <div
-                                                    class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                                                    <table class="min-w-full divide-y divide-gray-200 table-auto">
-                                                        <thead class="bg-gray-50">
-                                                            <tr>
-                                                                <th scope="col"
-                                                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                                                    Ip
-                                                                </th>
-                                                                <th scope="col"
-                                                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                                                    Port
-                                                                </th>
-                                                                <th scope="col"
-                                                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                                                    Packet length
-                                                                </th>
-                                                                <th scope="col"
-                                                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                                                    Date
-                                                                </th>
-                                                            </tr>
-                                                        </thead>
-                                                        <tbody class="bg-white divide-y divide-gray-200">
-                                                            @foreach ($protocols as $protocol)
-                                                                <tr>
-                                                                    <td class="px-6 py-4 whitespace-nowrap">
-                                                                        <div class="text-sm font-medium text-gray-900">
-                                                                            {{ $protocol->ip }}
-                                                                        </div>
-                                                                        <div class="text-sm text-gray-500">
-                                                                            {{ $protocol->country }}
-                                                                        </div>
-                                                                    </td>
-                                                                    <td class="px-6 py-4 whitespace-nowrap">
-                                                                        <span
-                                                                            class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                                                                            {{ $protocol->port }}
-                                                                        </span>
-                                                                    </td>
-                                                                    <td
-                                                                        class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                                                        {{ $protocol->pkt_len < 1024 ? $protocol->pkt_len . ' b' : number_format($protocol->pkt_len / 1024, 2, '.', '') . ' kb' }}
-                                                                    </td>
-                                                                    <td
-                                                                        class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                                                        {{ Carbon\Carbon::createFromTimestamp($protocol->ts)->toDateTimeString() }}
-                                                                    </td>
-                                                                </tr>
-                                                            @endforeach
-                                                        </tbody>
-                                                    </table>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
+                                    <livewire:protocol-traffic-list />
                                 </div>
                             </section>
                         </div>
