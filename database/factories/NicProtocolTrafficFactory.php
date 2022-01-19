2c6183afbe1a03867ee6c7251b2b5fdb5d6ed81f
Penfu <sypenfu@gmail.com>
2022-01-19 09:55:36 +0100

    Add country attribute to network protocol traffic collection

--- database/factories/NicProtocolTrafficFactory.php
+++ database/factories/NicProtocolTrafficFactory.php
@@ -13,11 +13,16 @@ class NicProtocolTrafficFactory extends Factory
      */
     public function definition()
     {
+        $countries = [
+            'Moldova', 'Armenia', 'Switzerland', 'Burundi', 'Morocco',
+            'Macao', 'Panama', 'Fiji', 'United States Minor Outlying Islands', 'China'
+        ];
         $ports = [22, 80, 443];
         $nic = "eth0";
 
         return [
             'ip' => $this->faker->ipv4(),
+            'country' => $this->faker->randomElement($countries),
             'pkt_len' => $this->faker->numberBetween(1, 2048), // from 1 to 2kb
             'ts' => $this->faker->dateTimeThisYear()->getTimestamp(),
             'port' => $this->faker->randomElement($ports),
