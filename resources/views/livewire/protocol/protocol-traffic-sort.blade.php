1827c82c88243c4883f2e4f255e9d4dcabc82c7c
Penfu <sypenfu@gmail.com>
2022-01-19 12:20:14 +0100

    Sort protocol traffic options

--- resources/views/livewire/protocol/protocol-traffic-sort.blade.php
+++ resources/views/livewire/protocol/protocol-traffic-sort.blade.php
@@ -1,21 +1,18 @@
-<div id="sort-list"
-    class="origin-top-right absolute right-0 mt-2 w-40 rounded-md shadow-2xl bg-white ring-1 ring-black ring-opacity-5 focus:outline-none hidden"
+<div id="sort-list" class="origin-top-right absolute right-0 mt-2 w-40 rounded-md shadow-2xl bg-white ring-1 ring-black ring-opacity-5 focus:outline-none hidden"
     role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
     <div class="py-1" role="none">
-        <a href="{{ route('bans.sort', ['param' => 'country']) }}"
-            class="font-medium text-gray-900 block px-4 py-2 text-sm" role="menuitem" tabindex="-1"
-            id="menu-item-0">Country</a>
-
-        <a href="{{ route('bans.sort', ['param' => 'port']) }}"
-            class="font-medium text-gray-900 block px-4 py-2 text-sm" role="menuitem" tabindex="-1"
-            id="menu-item-0">Port</a>
-
-        <a href="{{ route('bans.sort', ['param' => 'pkt_len	', 'type' => 'desc']) }}"
-            class="font-medium text-gray-900 block px-4 py-2 text-sm" role="menuitem" tabindex="-1"
-            id="menu-item-0">Packet Lenght</a>
-
-        <a href="{{ route('bans.sort', ['param' => 'last_ban', 'type' => 'desc']) }}"
-            class="font-medium text-gray-900 block px-4 py-2 text-sm" role="menuitem" tabindex="-1"
-            id="menu-item-0">Date</a>
+        <button wire:click="sort('country')" class="font-medium text-gray-900 block px-4 py-2 text-sm">
+            Country
+        </button>
+        <button wire:click="sort('port')" class="font-medium text-gray-900 block px-4 py-2 text-sm">
+            Port
+        </button>
+        <button wire:click="sort('pkt_len')" class="font-medium text-gray-900 block px-4 py-2 text-sm">
+            Packet Lenght
+        </button>
+        <button wire:click="sort('ts')" class="font-medium text-gray-900 block px-4 py-2 text-sm">
+            Date
+        </button>
     </div>
+    <script src="{{ asset('/js/dropdown.js') }}"></script>
 </div>
